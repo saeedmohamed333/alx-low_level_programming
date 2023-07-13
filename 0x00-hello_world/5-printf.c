9dc1ca6978cd51a2861ba3bbfb98bf58d4019902
saeedmohamed333 <sm6847437@gmail.com>
2023-07-13 16:41:17 -0700

    5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * description: prograsm prints using printf
+ *
+ * return 0 (success)
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
